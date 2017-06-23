16394d8352aec028cd17027adc63a58f2498ba20
john771124 <john771124@gmail.com>
2017-06-23 20:45:16 +0800

    Add files via upload

---
+++ Lab_Login/Secret.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="Secret.aspx.cs" Inherits="Secret" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
+    <p>
+    僅限會員觀看</p>
+<p>
+    <br />
+</p>
+<p>
+</p>
+</asp:Content>
+
---
+++ Lab_Login/Secret.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Secret : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["userName"] == null) {
+
+            Session["returnUrl"] = "Secret.aspx";
+            Response.Redirect("Login.aspx");
+
+            return;
+
+        }
+    }
+}
